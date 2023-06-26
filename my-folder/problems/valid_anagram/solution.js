85c9acae3f6f67a7038d1b81c537ec56fa1b6ca2
Pavle <iampavle.sync@gmail.com>
2023-06-26 00:14:57 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

---
+++ my-folder/problems/valid_anagram/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+
+    const dict = {};
+
+    for(let i = 0; i < s.length; i++) {
+        dict[s.at(i)] = dict[s.at(i)] ? (dict[s.at(i)] + 1) : 1;
+        dict[t.at(i)] = dict[t.at(i)] ? (dict[t.at(i)] - 1) : -1;
+    }
+
+    for(let frequency of Object.values(dict)) {
+        if(frequency !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
