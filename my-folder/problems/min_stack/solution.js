5e87093a4842eb06067fcd1fd8be9806bf0585c2
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-07-26 10:35:42 +0000

    Sync LeetCode submission - Min Stack (javascript)

---
+++ my-folder/problems/min_stack/solution.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+
+        const min = this.minStack.at(-1) ?? Number.MAX_VALUE;
+        if(val < min) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(min);
+        }
+    }
+
+    pop() {
+        this.minStack.pop();
+        return this.stack.pop();
+    }
+
+    top() {
+        return this.stack.at(-1);
+    }
+
+    getMin() {
+        return this.minStack.at(-1);
+    }
+}
+
+// /** 
+//  * Your MinStack object will be instantiated and called as such:
+//  * var obj = new MinStack()
+//  * obj.push(val)
+//  * obj.pop()
+//  * var param_3 = obj.top()
+//  * var param_4 = obj.getMin()
+//  */
