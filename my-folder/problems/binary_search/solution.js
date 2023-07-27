506e1e841da36a08001f8b72f0c148f9d8a814ed
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-07-27 09:50:13 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- my-folder/problems/binary_search/solution.js
+++ my-folder/problems/binary_search/solution.js
@@ -3,22 +3,24 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let l = 0, r = nums.length - 1, index;
+var search = function (nums, target) {
+  let l = 0,
+    r = nums.length - 1,
+    index;
 
-    while(l <= r) {
-        index = Math.floor((l + r) / 2);
+  while (l <= r) {
+    index = Math.floor((l + r) / 2);
 
-        if(nums[index] === target) {
-           return index;
-       }
-
-       if(nums[index] < target) {
-           l = index + 1;
-       } else {
-           r = index - 1;
-       }
+    if (nums[index] === target) {
+      return index;
     }
 
-    return -1;
+    if (nums[index] < target) {
+      l = index + 1;
+    } else {
+      r = index - 1;
+    }
+  }
+
+  return -1;
 };
