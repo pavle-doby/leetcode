4d98ba6d80a1e93e298212eeef71fbcaa7659d57
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-07-28 12:01:14 +0000

    Sync LeetCode submission - Plus One (javascript)

---
+++ my-folder/problems/plus_one/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {        
+    function increment({digits, index, inc}) {
+        // when number exapndes [9,9]  -> [1,0,0]
+        if(index < 0) {
+            digits.unshift(0);
+            index = 0;
+        }
+
+        let resArray = [...(digits.at(index) + inc).toString()].map(Number);
+        
+        if(resArray.length === 1) {
+            digits[index] = resArray[0];
+            return digits;
+        } else {
+            digits[index] = resArray[1];
+            return increment({digits, index: index - 1, inc: resArray[0]});
+        }
+    }
+    
+    return increment({digits, index: digits.length - 1, inc: 1});
+};
