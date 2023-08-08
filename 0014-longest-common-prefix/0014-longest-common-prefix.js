3bfc9d89f00e8626536c883f3a452d73dc9d1b69
Pavle <iampavle.sync@gmail.com>
2023-08-08 20:38:38 +0200

    Time: 60 ms (61.42%), Space: 44 MB (16.71%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+    let prefix = "";
+    const smallestLen = Math.min(...strs.map(str => str.length));
+    
+    for(let i = 0 ; i < smallestLen; i++) {
+        if(!isSameChar(strs, i)) {
+            return prefix;
+        }
+        prefix += strs[0][i];            
+    }
+        
+    return prefix;
+};
+
+function isSameChar(strs, index) {
+    let check = ""
+    for(const str of strs) {
+        if(!check) {
+            check = str[index];
+            continue;
+        } 
+        
+        if(check !== str[index]) {
+            return false;
+        }
+    }
+    return true;
+}
