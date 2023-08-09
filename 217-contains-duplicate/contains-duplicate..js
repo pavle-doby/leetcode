bb9f62278f4a39867aa64efb052b8fbaab76714b
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:57 +0200

    Time: 74 ms (beats: 95.38%) | Memory: 53.9 MB (beats: 58.91%) | 217 - Contains Duplicate

---
+++ 217-contains-duplicate/contains-duplicate..js
@@ -0,0 +1,16 @@
+// Time: 74 ms (beats: 95.38%) | Memory: 53.9 MB (beats: 58.91%) | 217 - Contains Duplicate
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const dict = {};
+    let hasDuplicate = false;
+    for(let i=0; i < nums.length; i++) {
+        hasDuplicate = !!dict[nums[i]];
+        if(hasDuplicate) break; 
+        dict[nums[i]] = true;
+    }
+    return hasDuplicate;
+};
