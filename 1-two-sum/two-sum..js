64f999983a5927b1324fb17cd0f00ea271bc7925
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:48 +0200

    Time: 46 ms (beats: 99.33%) | Memory: 42.9 MB (beats: 36.70%) | 1 - Two Sum

---
+++ 1-two-sum/two-sum..js
@@ -0,0 +1,33 @@
+// Time: 46 ms (beats: 99.33%) | Memory: 42.9 MB (beats: 36.70%) | 1 - Two Sum
+
+/**
+ * [1,2,3,2]
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ // quicker alg
+function twoSum(nums, target) {
+    const dict = {};
+    let diff;
+    for(let i = 0; i < nums.length; i++) {
+        diff = target - nums[i];
+        if(diff in dict) {
+            return [i, dict[diff]];
+        }
+        dict[nums[i]] = i;
+    }
+ };
+
+// better memory complexity
+// function twoSum(nums, target) {
+//     let diff, j;
+//     for(let i = 0; i < nums.length; i++) {
+//         diff = target - nums[i];
+//         j = nums.indexOf(diff, i + 1);
+//         if(j !== -1) {
+//             return [i, j]
+//         }
+//     }
+// }
+
