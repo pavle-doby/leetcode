a1f85214c52365f2888d03ae0b3845de68bc08e5
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:44 +0200

    Time: 64 ms (beats: 99.57%) | Memory: 48.8 MB (beats: 89.98%) | 344 - Reverse String

---
+++ 344-reverse-string/reverse-string..js
@@ -0,0 +1,9 @@
+// Time: 64 ms (beats: 99.57%) | Memory: 48.8 MB (beats: 89.98%) | 344 - Reverse String
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s) {
+    return s.reverse();
+};
