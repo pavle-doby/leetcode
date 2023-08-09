ce34789a149e95538145bfb88d38b6069410b570
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:11:00 +0200

    Time: 63 ms (beats: 47.23%) | Memory: 42.6 MB (beats: 27.41%) | 122 - Best Time to Buy and Sell Stock II

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii..js
@@ -0,0 +1,19 @@
+// Time: 63 ms (beats: 47.23%) | Memory: 42.6 MB (beats: 27.41%) | 122 - Best Time to Buy and Sell Stock II
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+    let profit = 0;
+    
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i -1]) {
+            profit += (prices[i] - prices[i - 1]);
+        }
+    }
+    
+    return profit;
+};
+
+
