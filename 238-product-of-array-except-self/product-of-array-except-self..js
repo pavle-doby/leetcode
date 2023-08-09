f664999fd9b8d4c7860809a69fd731e57ca18f6d
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:11:13 +0200

    Time: 94 ms (beats: 82.03%) | Memory: 55.7 MB (beats: 28.77%) | 238 - Product of Array Except Self

---
+++ 238-product-of-array-except-self/product-of-array-except-self..js
@@ -0,0 +1,22 @@
+// Time: 94 ms (beats: 82.03%) | Memory: 55.7 MB (beats: 28.77%) | 238 - Product of Array Except Self
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function productExceptSelf(nums) {
+  const prefix = new Array(nums.length);
+  const suffix = new Array(nums.length);
+
+  for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+    prefix[i] = (prefix[i - 1] ?? 1) * nums[i];
+    suffix[j] = (suffix[j + 1] ?? 1) * nums[j];
+  }
+
+  const out = new Array(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    out[i] = (prefix[i - 1] ?? 1) * (suffix[i + 1] ?? 1);
+  }
+
+  return out;
+}
