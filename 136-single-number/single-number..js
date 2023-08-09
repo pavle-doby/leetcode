1ae274fd2cdd55f822b7edd546acc135578fe229
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:21 +0200

    Time: 47 ms (beats: 98.96%) | Memory: 46.9 MB (beats: 31.24%) | 136 - Single Number

---
+++ 136-single-number/single-number..js
@@ -0,0 +1,20 @@
+// Time: 47 ms (beats: 98.96%) | Memory: 46.9 MB (beats: 31.24%) | 136 - Single Number
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const set = new Set();
+    
+    for(const num of nums) {
+        if(set.has(num)) {
+            set.delete(num);
+        }
+        else {
+            set.add(num);
+        }
+    }
+    
+    return [...set][0];
+};
