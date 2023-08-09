8c0ef706dfa0501dec4721c84daad8e82b5493ec
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:39 +0200

    Time: 67 ms (beats: 75.36%) | Memory: 44.1 MB (beats: 87.42%) | 125 - Valid Palindrome

---
+++ 125-valid-palindrome/valid-palindrome..js
@@ -0,0 +1,22 @@
+// Time: 67 ms (beats: 75.36%) | Memory: 44.1 MB (beats: 87.42%) | 125 - Valid Palindrome
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const alphanumerical = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  let left = 0;
+  let right = alphanumerical.length - 1;
+
+  while (left < right) {
+    if (alphanumerical[left] !== alphanumerical[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+};
