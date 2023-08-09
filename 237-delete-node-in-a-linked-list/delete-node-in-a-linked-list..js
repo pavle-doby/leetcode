c2e3889e155ed2bd0c098bfb60162f31d60eda33
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:35 +0200

    Time: 60 ms (beats: 80.23%) | Memory: 43.8 MB (beats: 85.20%) | 237 - Delete Node in a Linked List

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list..js
@@ -0,0 +1,19 @@
+// Time: 60 ms (beats: 80.23%) | Memory: 43.8 MB (beats: 85.20%) | 237 - Delete Node in a Linked List
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+// node: 5
+// [1,2,5,6,7] => [1,2,6,7]
+function deleteNode(node) {    
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
