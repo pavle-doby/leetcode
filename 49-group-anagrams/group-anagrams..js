da0d9914fbf0b820a3246ccbda5899fdc121e6b3
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:11:33 +0200

    Time: 116 ms (beats: 81.70%) | Memory: 51.6 MB (beats: 99.05%) | 49 - Group Anagrams

---
+++ 49-group-anagrams/group-anagrams..js
@@ -0,0 +1,21 @@
+// Time: 116 ms (beats: 81.70%) | Memory: 51.6 MB (beats: 99.05%) | 49 - Group Anagrams
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const anagramMap = new Map();
+  let sortedStr = "";
+
+  strs.forEach((str) => {
+    sortedStr = str.split("").sort().join("");
+    if (anagramMap.has(sortedStr)) {
+      anagramMap.get(sortedStr).push(str);
+    } else {
+      anagramMap.set(sortedStr, [str]);
+    }
+  });
+
+  return [...anagramMap.values()];
+};
