9b0019e4193cffaa92655f71f2ec9b351993ed09
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:23 +0200

    Time: 59 ms (beats: 99.69%) | Memory: 45.5 MB (beats: 86.25%) | 387 - First Unique Character in a String

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string..ts
@@ -0,0 +1,19 @@
+// Time: 59 ms (beats: 99.69%) | Memory: 45.5 MB (beats: 86.25%) | 387 - First Unique Character in a String
+
+function firstUniqChar(str: string): number {
+    const set = new Set<string>();
+
+    for(let i = 0; i < str.length; i++) {
+        if(set.has(str[i])) {
+            continue;
+        }
+
+        if(str.indexOf(str[i], i + 1) === -1) {
+            return i;
+        } else {
+            set.add(str[i])
+        }
+    }
+
+    return -1;
+};
