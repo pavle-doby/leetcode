af7fe28dd10403ecd80a233ec14faff3d553f540
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:18 +0200

    Time: 62 ms (beats: 51.56%) | Memory: 44.6 MB (beats: 8.39%) | 14 - Longest Common Prefix

---
+++ 14-longest-common-prefix/longest-common-prefix..js
@@ -0,0 +1,34 @@
+// Time: 62 ms (beats: 51.56%) | Memory: 44.6 MB (beats: 8.39%) | 14 - Longest Common Prefix
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+    let prefix = "";
+    const smallestLen = Math.min(...strs.map(str => str.length));
+    
+    for(let i = 0 ; i < smallestLen; i++) {
+        if(!isSameChar(strs, i)) {
+            return prefix;
+        }
+        prefix += strs[0][i];            
+    }
+        
+    return prefix;
+};
+
+function isSameChar(strs, index) {
+    let check = ""
+    for(const str of strs) {
+        if(!check) {
+            check = str[index];
+            continue;
+        } 
+        
+        if(check !== str[index]) {
+            return false;
+        }
+    }
+    return true;
+}
