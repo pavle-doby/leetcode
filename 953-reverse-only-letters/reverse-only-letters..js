1b51b632a736af615416af40e089214bf31e21df
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:10:28 +0200

    Time: 45 ms (beats: 97.96%) | Memory: 41.4 MB (beats: 98.41%) | 953 - Reverse Only Letters

---
+++ 953-reverse-only-letters/reverse-only-letters..js
@@ -0,0 +1,23 @@
+// Time: 45 ms (beats: 97.96%) | Memory: 41.4 MB (beats: 98.41%) | 953 - Reverse Only Letters
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function reverseOnlyLetters(str) {
+    const pureStrArr = str.split("").filter(isEnglishLetter).reverse();
+    
+    for(let i = 0; i < str.length; i++) {
+        if(!isEnglishLetter(str[i])) {
+            pureStrArr.splice(i, 0, str[i])
+        }
+    }
+
+    return pureStrArr.join("")
+};
+
+function isEnglishLetter(char) {
+    const isLowerCaseLetter = char >= "a" && char <= "z";
+    const isUpperCaseLetter = char >= "A" && char <= "Z";
+    return isLowerCaseLetter || isUpperCaseLetter
+}
