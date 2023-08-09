62e8532ff7da5d6ad0bb51b283b724ffc1fd6f53
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-09 02:11:31 +0200

    Time: 55 ms (beats: 98.65%) | Memory: 44.5 MB (beats: 89.95%) | 347 - Top K Frequent Elements

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements..js
@@ -0,0 +1,18 @@
+// Time: 55 ms (beats: 98.65%) | Memory: 44.5 MB (beats: 89.95%) | 347 - Top K Frequent Elements
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  const map = new Map();
+
+  nums.forEach((num) => {
+    map.set(num, (map.get(num) || 0) + 1);
+  });
+
+  return [...map.keys()]
+    .sort((aKey, bKey) => map.get(bKey) - map.get(aKey))
+    .slice(0, k);
+};
